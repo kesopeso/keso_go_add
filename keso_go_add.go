208dfdbe0c9bbff119f7e1269d8632daa6a6b584
Dorde Kesic <dordekesic.keso@gmail.com>
2024-10-01 14:24:16 +0200

    adds comments

--- keso_go_add.go
+++ keso_go_add.go
@@ -1,5 +1,12 @@
+// Package keso_go_add is a simple go package with a single function.
+// Its purpose is to learn go module management system.
 package keso_go_add
 
+// Add sums up two numers.
+//
+// It takes two numbers as input parameters.
+// You can learn more about addition mathematical function [HERE],
+// [HERE]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
